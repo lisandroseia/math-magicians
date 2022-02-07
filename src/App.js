362dcfbc483f92fef2690da814714ac7e7530f72
lisandroseia <lisandroseia11@gmail.com>
2022-02-07 16:47:32 -0300

    Fix linters

--- src/App.js
+++ src/App.js
@@ -6,10 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         nothing made yet,only setup
-        </p>
-        
+        <p>nothing made yet,only setup</p>
       </header>
     </div>
   );
