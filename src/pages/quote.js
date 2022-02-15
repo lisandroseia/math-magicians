fa37b1830d8366b5a1e664f2db061921067aa648
lisandroseia <lisandroseia11@gmail.com>
2022-02-15 18:19:03 -0300

    Finish styling

---
+++ src/pages/quote.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './quote.css';
+
+const Quote = () => (
+  <div className="quote-cont">
+    <cite>
+      Mathematics is not about numbers, equations, computation or algorithms; it
+      is about understanding
+    </cite>
+    <i>-William Paul Thurston</i>
+  </div>
+);
+
+export default Quote;
